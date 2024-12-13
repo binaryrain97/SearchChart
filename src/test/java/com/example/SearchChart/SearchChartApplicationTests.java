44f2a2520059ecc9b2ecb403fc6ffb6840f9c74a
binaryrain97 <binaryrain97@gmail.com>
2024-12-13 11:36:09 +0900

    setting

---
+++ src/test/java/com/example/SearchChart/SearchChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SearchChart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
